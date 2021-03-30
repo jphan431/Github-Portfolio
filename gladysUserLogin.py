59abe78ba78c858099cc1e5a9e907c339f90ea4a
jphan431 <77815882+jphan431@users.noreply.github.com>
2021-03-29 22:33:12 -0700

    Add files via upload

---
+++ gladysUserLogin.py
@@ -0,0 +1,31 @@
+import io
+
+"""
+	Student: Gabriel Solomon
+	Module: gladysUserLogin
+	Description: This module does …
+"""
+
+
+def login():
+	username="Jim Phan"
+	password="test1" 
+	
+	userInput = input("Enter a Login")
+	
+	
+	userInput = input("Enter a Password:")
+	
+	"""
+		document your function definition here. what does it do?
+	"""
+
+	"""
+		delete the remaining code *in this function* and replace it with
+		your own code. add more code to do what the assignment asks of you.
+	"""
+	
+	emailAddress = "malcomx@mecca.com"
+
+
+	return emailAddress
